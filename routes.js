dee477594cd922f70d32734ba93a121be4387611
Your Name <you@example.com>
2018-06-21 17:17:41 +0530

    login commit

--- routes.js
+++ routes.js
@@ -6,24 +6,38 @@ var path			= require("path");
 var AuthenticationClient = require('auth0').AuthenticationClient;
 
 var auth0 = new AuthenticationClient({
-  domain: '{YOUR_ACCOUNT}.auth0.com',
-  clientId: '{OPTIONAL_CLIENT_ID}'
+  domain: 'hexaesbot.auth0.com',
+  clientId: 'tZ5fyaM3PEQhi7pwrQvfJP9VWRGrQ5Ke'
 });
 
 router.post('/botHandler',function(req, res){
 	console.log(JSON.stringify(req.body));
+		console.log(req.body.queryResult.parameters.empid);
+		console.log(req.body.queryResult.parameters.phone);
 })
 
 var data = {
-  phone_number: '{PHONE}'
+  phone_number: '8500050085'
 };
 
 auth0.passwordless.sendSMS(data, function (err) {
   if (err) {
     // Handle error.
+  }else{
+	  
   }
 });
 
+var data = {
+  username: '8500050085',
+  password: '{VERIFICATION_CODE}'
+};
+
+auth0.passwordless.signIn(data, function (err) {
+  if (err) {
+    // Handle error.
+  }
+});
 
 
 module.exports = router;
