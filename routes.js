e66a01dc1d42afa26f8ad567c7a5c40de4db9855
39754-Hari <35679989+39754-Hari@users.noreply.github.com>
2018-06-21 18:22:48 +0530

    For OTP

--- routes.js
+++ routes.js
@@ -12,23 +12,54 @@ var auth0 = new AuthenticationClient({
 
 router.post('/botHandler',function(req, res){
 	console.log(JSON.stringify(req.body));
+	if(req.body.result.action === 'input.welcome'){
+		var resObj = {};
 		console.log(req.body.queryResult.parameters.empid);		
 		var data = {
-			phone_number: '+917200050085'
+			phone_number: '+919626649195'
 		};
 
 		auth0.passwordless.sendSMS(data, function (err) {
 		  if (err) {
 			// Handle error.
 		  }else{
-			  console.log(data);
+				console.log(data);
+				resObj= {
+					"speech": "",
+					"messages": [
+						{
+							"type": "simple_response",
+							"platform": "google",
+							"textToSpeech": "Please enter the OTP sent to your phone",
+							"displayText": "Please enter the OTP sent to your phone"
+						}
+					]
+				}
 		  }
 		});
+		res.json(resObj);
+	}
+	else if(req.body.result.action === 'verify_otp'){
+		console.log(req.body.queryResult.parameters.otp);		
+		var data = {
+			username: '+919626649195',
+			password: req.body.queryResult.parameters.otp
+		};
+
+		auth0.passwordless.signIn(data, function (err) {
+			if (err) {
+				// Handle error.
+			}
+			else{
+				console.log('After OTP');
+				console.log(data);
+			}
+		});
+	}
 })
 
 
-
-var data = {
+/*var data = {
   username: '+918500050085',
   password: '{VERIFICATION_CODE}'
 };
@@ -37,7 +68,8 @@ auth0.passwordless.signIn(data, function (err) {
   if (err) {
     // Handle error.
   }
-});
+});*/
+
 
 
 module.exports = router;
