fac819748625b1986fbe68940fd186837b395fce
Your Name <you@example.com>
2018-06-22 15:37:33 +0530

    login commit

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ module.exports = {
 	employees:{
 		"39781":"8500050085",
 		"39754":"9626649195"
-	}
+	},
+	"smsApi":"http://smsapi.24x7sms.com/api_2.0/SendSMS.aspx?APIKEY=ZY2nHm2RiIC&MobileNo=phonenumber&SenderID=TESTIN&Message=Dear+empid%2c+Otpnumber+is+the+OTP+for+your+signin+Enter+the+OTP+to+verify+your+mobile+number+OTP+is+valid+only+for+the+next+30++minutes.&ServiceName=TEMPLATE_BASED"
 }
 
--- routes.js
+++ routes.js
@@ -4,11 +4,30 @@ var fs 				= require("fs");
 var request			= require('request');
 var config			= require('./config.js');
 var path			= require("path");	
-var AuthenticationClient = require('auth0').AuthenticationClient;
+
 
 router.post('/validateUser',function(req, res){
-	res.json({status:true}).end();
+	var emps = config.employees;
+	if(typeof(emps[req.body.username])!='undefined'){
+		var smsApi = config.smsApi.replace('phonenumber',emps[req.body.username]);	
+		smsApi = smsApi.replace('Otpnumber',45627);
+		request(smsApi, function (error, response, body) {
+			console.log(body);
+			res.sendFile(path.join(__dirname, '../public', 'verifyOtp.html?token=TKN'+emps[req.body.username].reverse()));			
+		});	
+	}			
 });
+
+
+var simpleResponse = function(response, responseText){
+	response.payload.google.richResponse.items.push({
+		"simpleResponse": {
+			"textToSpeech": responseText,
+			"displayText": responseText
+		}
+	});		
+	return response;
+}
 
 module.exports = router;
 
