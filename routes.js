340ca415d3588f941c1aed282c92689a299ed319
Your Name <you@example.com>
2018-06-22 15:52:42 +0530

    login commit

--- routes.js
+++ routes.js
@@ -5,20 +5,32 @@ var request			= require('request');
 var config			= require('./config.js');
 var path			= require("path");	
 
-
+var Otps ={};
 router.post('/validateUser',function(req, res){
 	var emps = config.employees;
 	if(typeof(emps[req.body.username])!='undefined'){
 		var smsApi = config.smsApi.replace('phonenumber',emps[req.body.username]);	
 		smsApi = smsApi.replace('Otpnumber',45627);
+		otps[emps[req.body.username]] = 45627;
 		request(smsApi, function (error, response, body) {
-			console.log(body);
-			res.sendFile(path.join(__dirname, '../public', 'verifyOtp.html?token=TKN'+emps[req.body.username].reverse()));			
+			console.log(body);			
+			res.sendFile(path.join(__dirname, '../public', 'verifyOtp.html?token=TKN'+emps[req.body.username].split("").reverse().join("")));			
 		});	
-	}			
+	}else{
+		res.status(400);
+		res.json({status:false}).end();
+	}		
 });
 
-
+router.post('/validateOtp',function(req, res){
+	if(otps[req.body.phone]==req.body.otp){
+		res.status(200);
+		res.json({status:true}).end();	
+	}else{
+		res.status(400);
+		res.json({status:false}).end();	
+	}		
+});
 var simpleResponse = function(response, responseText){
 	response.payload.google.richResponse.items.push({
 		"simpleResponse": {
